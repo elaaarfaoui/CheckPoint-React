29bab46799a42af67b95a4be9468ca7acbc129dd
elaaarfaoui <elaa.arfaoui1@gmail.com>
2021-01-07 12:32:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { Form, Button } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form className="FormDiv">
+        <div className="title"> 
+      <Form.Label>Contact</Form.Label>
+      </div>
+          <Form.Group controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Your name" />
+          <Form.Label>LastName</Form.Label>
+          <Form.Control type="Lastname" placeholder="Your Lastname" />
+          <div className="gender"> 
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Gender</Form.Label>
+            <Form.Control as="select">
+              <option>Male</option>
+              <option>Female</option>
+            </Form.Control>
+          </Form.Group>
+          </div>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Text className="text-muted">
+    </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        
+        <div className="btnsubmit">  
+        <Button variant="primary" type="submit"> Submit</Button>
+        </div>
+      </Form>
+
+
+
     </div>
   );
 }
